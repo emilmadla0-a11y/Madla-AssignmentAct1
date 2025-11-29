7570cd1584873857b7bd0c39166af03b2ec051f6
emilmadla0-a11y <emilmadla0@gmail.com>
2025-11-29 20:56:12 +0800

    Add files via upload

---
+++ produktoInfo.php
@@ -0,0 +1,6 @@
+<?php
+    $produkto = "L Black Tshirt"; //name of the product
+    $descri = "Itong Tshirt na to ay for men only at pinapakita nito <br> ang pagka dilim na nagbibigay nang +100 aura";
+    $presyo = 299.99; 
+    $piraso = 6;
+?>
