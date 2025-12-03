72a31877f2bcc41aa2d3b3421c73c2d7c55d982e
emilmadla0-a11y <emilmadla0@gmail.com>
2025-12-03 19:35:25 +0800

    Updated produktoInfo.php

--- produktoInfo.php
+++ produktoInfo.php
@@ -1,6 +1,24 @@
 <?php
     $produkto = "L Black Tshirt"; //name of the product
-    $descri = "Itong Tshirt na to ay for men only at pinapakita nito <br> ang pagka dilim na nagbibigay nang +100 aura";
+    $descri = "Itong Tshirt na to ay for men only at pinapakita nito <br> ang pagka dilim at dating na nagbibigay nang +100 aura";
     $presyo = 299.99; 
-    $piraso = 6;
+    $piraso = 3;
+
+    //total
+$totality = $presyo * $piraso;
+
+//conditional statement
+if ($piraso > 5) {
+    $discount = 0.10;
+    $hulingTotality = $totality - ($totality * $discount);
+} else {
+    $discount = 0;
+    $hulingTotality = $totality;
+}
+//loop sample
+$listahanProdukto = "";
+for ($i = 1; $i <= $piraso; $i++) {
+    $listahanProdukto .= "Item #$i added to cart<br>";
+}
+
 ?>
